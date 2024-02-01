e6d2c306ccdac2a11ec94c9f74c32e5e17c4ef17
GandlaGowthami30 <144326694+GandlaGowthami30@users.noreply.github.com>
2024-02-01 21:34:58 +0530

    Add files via upload

---
+++ emailslicer.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/home/AppData/Local/Programs/Python/Python37-32/rktechnology/emailslicer.py 
+Enter your email address:rktechnologies1922@gmail.com
+Your username = rktechnologies1922
+Your domain=gmail.com
+>>> 
